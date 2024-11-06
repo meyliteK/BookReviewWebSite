495166da29ce58d63a83ce3b6d02b53a0a7ef517
meylitek <meylitek@MEYLITEK>
2024-11-06 18:35:38 +0300

    Proje dosyası ekle.

---
+++ KitapUygulaması.Data/Entities/Kitap.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitapUygulaması.Data.Entities
+{
+    public class Kitap : BaseEntity<int>
+    {
+
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Director { get; set; } //list olabilir
+
+        public string Scenarist { get; set; }
+
+        public string Cast { get; set; } // liste olucak düzelt
+
+        public string Image { get; set; }
+
+        public float Star { get; set; }
+
+        public int RunTime { get; set; }
+
+        public string Overview { get; set; }
+
+        public int Date { get; set; }
+
+        public List<Rewiew>? Rewiews { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
